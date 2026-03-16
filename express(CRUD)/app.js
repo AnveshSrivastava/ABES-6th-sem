d662dd0b948ad71717f60d4efac85aad41386e9a
Anvesh Srivastava <anveshsrivastava9@gmail.com>
2026-03-16 13:07:04 +0530

    Class3

--- express(CRUD)/app.js
+++ express(CRUD)/app.js
@@ -4,46 +4,73 @@ const port = 3000;
 
 app.use(express.json());
 
-let users = [];
+let users = [{
+    "id": 1,
+    "name": "Jon Kaisen",
+    "email": "mot@gmail.com"
+  },
+  {
+    "id": 2,
+    "name": "Jon Kaisen",
+    "email": "mot@gmail.com"
+  },
+  {
+    "id": 3,
+    "name": "Jon Kaisen",
+    "email": "mot@gmail.com"
+  },
+  {
+    "id": 4,
+    "name": "Jon Kaisen",
+    "email": "mot@gmail.com"
+  },
+  {
+    "id": 5,
+    "name": "Jon Kaisen",
+    "email": "mot@gmail.com"
+  }];
 
-// Create a new user
-app.post('/users', (req, res) => {
-    const { name, email } = req.body;
-    const id = users.length + 1;
-    const newUser = { id, name, email };
-    users.push(newUser);
-    res.status(201).json(newUser);
-}
-);
-// Get all users 
+// app.post('/users', (req, res) => {
+//     const { name, email } = req.body;
+//     const id = users.length + 1;
+//     const newUser = { id, name, email };
+//     users.push(newUser);
+//     res.status(201).json(newUser);
+// }
+// );
+
+app.post('/add', (req, res)=> {
+    const newStudent = {
+        id : users.length + 1,
+        ...req.body
+    }
+    users.push(newStudent);
+    res.status(201).json({message: 'Student added successfully', student: users});
+})
+
 app.get('/users', (req, res) => {
     res.json(users);
 });
-
-// Get a user by ID
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', (req, res) =>{
     const user = users.find(u => u.id === parseInt(req.params.id));
-    if (user) {
+    if(user) {
         res.json(user);
     } else {
         res.status(404).json({ message: 'User not found' });
     }
-});
+})
+// app.put('/users/:id', (req, res) => {
+//     const user = users.find(u => u.id === parseInt(req.params.id));
+//     if (user) {
+//         const { name, email } = req.body;
+//         user.name = name || user.name;
+//         user.email = email || user.email;
+//         res.json(user);
+//     } else {
+//         res.status(404).json({ message: 'User not found' });
+//     }
+// });
 
-// Update a user by ID
-app.put('/users/:id', (req, res) => {
-    const user = users.find(u => u.id === parseInt(req.params.id));
-    if (user) {
-        const { name, email } = req.body;
-        user.name = name || user.name;
-        user.email = email || user.email;
-        res.json(user);
-    } else {
-        res.status(404).json({ message: 'User not found' });
-    }
-});
-
-// Delete a user by ID
 app.delete('/users/:id', (req, res) => {
     const index = users.findIndex(u => u.id === parseInt(req.params.id));
     if (index !== -1) {
